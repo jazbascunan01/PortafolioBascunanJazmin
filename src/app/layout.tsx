7460a679062347eddb45dc14ca72bafa2132c220
Jazmin Bascuñan <jazbascunan01@gmail.com>
2025-08-06 19:26:53 -0300

    Limpio codigo y agrego icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es" style={{ scrollBehavior: 'smooth' }}>
+      <html lang="es">
     <head>
         {/*<link rel="icon" href="images/image-_3_.ico"/>*/}
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
